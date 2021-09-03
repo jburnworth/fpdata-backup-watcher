6f063a5d21a8de24ea1731df9a4d10d9120c0758
James Burnworth <j.j.burnworth@gmail.com>
2021-09-02 20:54:42 -0600

    Reading contents of zip files into strings.
    Added serde to parse anything and everything I guess.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use postgres::{Client, NoTls, Error};
-use std::io;
-use std::fs;
+use std::path::Path;
+use std::io::BufReader;
+use std::fs::File;
+use serde::Deserialize;
+use std::io::prelude::*;
 
 fn main() {
     let mut client = Client::connect("postgresql://postgres:jam@localhost:5432/prodBackupsDBbbb", NoTls);
@@ -9,12 +12,12 @@ fn main() {
     println!("Watching for backups");
 
     let fname = std::path::Path::new("/home/james/backups/17000-21009352412.zip");
-    let file = fs::File::open(&fname).unwrap();
+    let file = File::open(&fname).unwrap();
 
     let mut zippy = zip::ZipArchive::new(file).unwrap();
 
     for i in 0..zippy.len() {
-        let file = zippy.by_index(i).unwrap();
+        let mut file = zippy.by_index(i).unwrap();
         let outpath = match file.enclosed_name() {
             Some(path) => path.to_owned(),
             None => continue,
@@ -22,6 +25,24 @@ fn main() {
         // Only print files, not directories
         if ! file.name().ends_with('/') {
             println!("Found file: {}", outpath.display());
+            println!("{}", file.name());
+            if file.name().ends_with(".json") {
+                // Read file into string
+                let mut contents = String::new();
+                file.read_to_string(&mut contents).unwrap();
+                println!("{}", contents);
+                // parse string with serde
+                backup_parsing::parse_json(file.name());
+            }
         }
+    }
+}
+
+mod backup_parsing {
+    pub fn parse_json<P: AsRef<std::path::Path>>(path: P) {
+        println!("Watch out, we're parsing jsons {:#?}", path.as_ref());
+        // let me = std::fs::File::open(path);
+        // let reader = std::io::BufReader::new(me);
+
     }
 }
