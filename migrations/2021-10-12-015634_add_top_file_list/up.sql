ea9d922ff1778bfc69375520b921c8d7cf74dacb
James Burnworth <j.j.burnworth@gmail.com>
2021-12-19 11:08:01 -0700

    Add diesel ORM.
    Let's see if I remember how to use it.

---
+++ migrations/2021-10-12-015634_add_top_file_list/up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE top (
+	serialNumber INTEGER NOT NULL PRIMARY KEY,
+	configGeneration INTEGER NOT NULL
+)
+CREATE TABLE machineConfig (
+	machineConfigID integer NOT NULL PRIMARY KEY,
+)
+
+
+machine_configuration/
+machine_configuration/system-connections/
+machine_configuration/system-connections/Ethernet Connection
+machine_configuration/20-wired.network
+machine_configuration/serialNumber
+machine_configuration/machine-axis.json
+machine_configuration/config.ini
+machine_configuration/machine-config.json
+machine_configuration/sw-versions
+vision_configuration/
+vision_configuration/INTRINSICS.yml
+vision_configuration/measured_target.yml
+vision_configuration/EXTRINSICS_470F461C3E24A9D2.yml
+vision_configuration/QUICKRECALWORLDPOINTS_470F461C3E24A9D2.yml
